867f3a62ab5739e32c002af624f1b74e3575df6f
bgroveben <bgroveben@gmail.com>
2018-12-12 01:07:50 -0800

    Remove blabber clone

--- app/models/blab.rb
+++
@@ -1,5 +0,0 @@
-class Blab < ActiveRecord::Base
-	attr_accessible :text
-	validates_presence_of :text
-	belongs_to :user
-end
--- db/migrate/20130523071153_create_blabs.rb
+++
@@ -1,9 +0,0 @@
-class CreateBlabs < ActiveRecord::Migration
-  def change
-    create_table :blabs do |t|
-      t.string :text
-      t.integer :user_id
-      t.timestamps
-    end
-  end
-end
