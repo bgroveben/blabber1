85819b1638ba97e83cd2099e45812b1d2b78f948
bgroveben <bgroveben@gmail.com>
2018-12-28 00:26:56 -0800

    Write the first view -- register the user

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Web Applications built with Python and Flask
 
 [Flask Tutorial](http://flask.pocoo.org/docs/1.0/tutorial/) from the docs.  
-[I'm here](http://flask.pocoo.org/docs/1.0/tutorial/views/#the-first-view-register) 
+[I'm here](http://flask.pocoo.org/docs/1.0/tutorial/views/#login) 
 
 [Flask API](https://www.flaskapi.org/)
 
--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -6,3 +6,32 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET, POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = "Sorry, you're going to need a username."
+        elif not password:
+            error = "Sorry, you're going to need a password."
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = "User {}? You're already registered. Welcome back.".format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
